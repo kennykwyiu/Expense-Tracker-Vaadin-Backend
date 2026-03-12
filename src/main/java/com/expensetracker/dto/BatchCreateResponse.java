f89865dc7c56dfb932988c51ee6692441387bfe2
kennyyiu <kenny.kwyiu@gmail.com>
2026-03-12 20:20:04 +0800

    add BatchCreateResponse

---
+++ src/main/java/com/expensetracker/dto/BatchCreateResponse.java
@@ -0,0 +1,16 @@
+package com.expensetracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.util.List;
+
+/**
+ * Response DTO for batch create operation.
+ * Includes created expenses and any failures with error details.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BatchCreateResponse {
+}
