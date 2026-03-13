309cb557881082352fb909e8b5bc44943ca59d50
kennyyiu <kenny.kwyiu@gmail.com>
2026-03-14 00:06:26 +0800

    add ListExpensesResponse

---
+++ src/main/java/com/expensetracker/dto/ListExpensesResponse.java
@@ -0,0 +1,17 @@
+package com.expensetracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.util.List;
+
+/**
+ * Response DTO for listing expenses.
+ * Includes array of expenses, totals, and month/year context.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ListExpensesResponse {
+}
