e484a65e9d400633adbd4f799ec05ddffc51e575
kennyyiu <kenny.kwyiu@gmail.com>
2026-03-16 23:31:00 +0800

    add ListExpensesRequest - variables

--- src/main/java/com/expensetracker/dto/ListExpensesRequest.java
+++ src/main/java/com/expensetracker/dto/ListExpensesRequest.java
@@ -13,4 +13,13 @@ import jakarta.validation.constraints.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ListExpensesRequest {
+    @NotNull(message = "Year is required")
+    @Min(value = 1900, message = "Year must be 1900 or later")
+    @Max(value = 2100, message = "Year must be 2100 or earlier")
+    private Integer year;
+
+    @NotNull(message = "Month is required")
+    @Min(value = 1, message = "Month must be between 1 and 12")
+    @Max(value = 12, message = "Month must be between 1 and 12")
+    private Integer month;
 }
