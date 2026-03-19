92616aed3f5c762d71b6dc4e1aaa4706af080126
kennyyiu <kenny.kwyiu@gmail.com>
2026-03-19 23:15:29 +0800

    add BatchFailure

---
+++ src/main/java/com/expensetracker/dto/BatchFailure.java
@@ -0,0 +1,15 @@
+package com.expensetracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * DTO representing a failed expense in a batch operation.
+ * Includes the index of the failed item and error message.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BatchFailure {
+}
