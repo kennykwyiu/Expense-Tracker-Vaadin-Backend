464bb31fd2b41f3334b4996e4dfcaec0c22e421e
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-04 00:10:37 +0800

    add MonthlyBalance - add comment

--- src/main/java/com/expensetracker/entity/MonthlyBalance.java
+++ src/main/java/com/expensetracker/entity/MonthlyBalance.java
@@ -1,4 +1,7 @@
 package com.expensetracker.entity;
-
+/**
+ * MonthlyBalance entity representing user's financial summary for each month.
+ * Tracks: last month balance, income, expense budget, and calculated current balance.
+ */
 public class MonthlyBalance {
 }
