1a8a74539d80668b7221497b73acde6c3694e72f
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-05 00:17:21 +0800

    add UpdateMonthlyBalanceRequest - add comment and annotations

--- src/main/java/com/expensetracker/dto/UpdateMonthlyBalanceRequest.java
+++ src/main/java/com/expensetracker/dto/UpdateMonthlyBalanceRequest.java
@@ -1,4 +1,14 @@
 package com.expensetracker.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Request DTO for updating monthly balance
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UpdateMonthlyBalanceRequest {
 }
