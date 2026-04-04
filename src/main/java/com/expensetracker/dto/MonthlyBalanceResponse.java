e2c1cc2c011d30f7538bec2ac48282ec60376cb7
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-05 00:16:16 +0800

    add MonthlyBalanceResponse - add comment and annotations

--- src/main/java/com/expensetracker/dto/MonthlyBalanceResponse.java
+++ src/main/java/com/expensetracker/dto/MonthlyBalanceResponse.java
@@ -1,4 +1,14 @@
 package com.expensetracker.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Response DTO for monthly balance information
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MonthlyBalanceResponse {
 }
