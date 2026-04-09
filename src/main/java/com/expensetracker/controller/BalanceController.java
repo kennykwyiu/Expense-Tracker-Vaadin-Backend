fd26f36dfd9c9892e5ea7f6a08de407cc5f99a86
kennyyiu <kenny.kwyiu@gmail.com>
2026-04-10 00:00:29 +0800

    add BalanceController - add annotations and comment

--- src/main/java/com/expensetracker/controller/BalanceController.java
+++ src/main/java/com/expensetracker/controller/BalanceController.java
@@ -1,4 +1,17 @@
 package com.expensetracker.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * REST Controller for monthly balance management endpoints.
+ * All endpoints are prefixed with /api/balance
+ */
+@RestController
+@RequestMapping("/balance")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:8081")
 public class BalanceController {
 }
